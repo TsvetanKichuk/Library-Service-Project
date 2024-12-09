0e38d5ca675df0f6839d19e858815a75d27579c8
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-09 20:43:21 +0100

    Add PaymentsViewSet to handle payment operations

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer
+from borrowing.models import Borrowing, Payments
+from borrowing.serializers import BorrowingSerializer, PaymentsSerializers
 
 
 class BorrowingViewSet(viewsets.GenericViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+
+class PaymentsViewSet(viewsets.GenericViewSet):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializers
