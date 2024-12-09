00772ae8e54207cdfcc25728c4dadacb1786129d
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-09 15:46:37 +0100

    Add serializer and permissions to BookViewSet

--- book_app/views.py
+++ book_app/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-
 from rest_framework.viewsets import GenericViewSet
 
 from book_app.models import Book
+from book_app.permissions import IsAdminOrIfAuthenticatedReadOnly
+from book_app.serializers import BookSerializer
 
 
 class BookViewSet(GenericViewSet):
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
