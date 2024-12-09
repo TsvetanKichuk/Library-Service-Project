1acc12e1ba10edc788c595f6dd8ea7d334896e7b
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-09 20:43:07 +0100

    Add PaymentsSerializer to serializers module

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from borrowing.models import Borrowing
+from borrowing.models import Borrowing, Payments
 
 
 class BorrowingSerializer(serializers.Serializer):
     class Meta:
         model: Borrowing
+        fields = '__all__'
+
+
+class PaymentsSerializers(serializers.Serializer):
+    class Meta:
+        model: Payments
         fields = '__all__'
