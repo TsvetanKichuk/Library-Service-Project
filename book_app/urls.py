613af09fdb2d391024b029f25a24bf57825706e3
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-10 19:24:08 +0100

    Change URL route from "books" to "book"

--- book_app/urls.py
+++ book_app/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from book_app.views import BookViewSet
 
 router = routers.DefaultRouter()
-router.register("books", BookViewSet)
+router.register("book", BookViewSet)
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "book_app"
