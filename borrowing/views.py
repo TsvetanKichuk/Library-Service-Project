028495e68ce5d02350a64ff72e6b3acfd430f20e
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-10 07:34:16 +0100

    Optimize queryset retrieval with select_related

--- borrowing/views.py
+++ borrowing/views.py
@@ -5,10 +5,10 @@ from borrowing.serializers import BorrowingSerializer, PaymentsSerializers
 
 
 class BorrowingViewSet(viewsets.GenericViewSet):
-    queryset = Borrowing.objects.all()
+    queryset = Borrowing.objects.select_related("book, user")
     serializer_class = BorrowingSerializer
 
 
 class PaymentsViewSet(viewsets.GenericViewSet):
-    queryset = Payments.objects.all()
+    queryset = Payments.objects.select_related("borrowing_id")
     serializer_class = PaymentsSerializers
