76d92d21df3c3f4808aa73e81f85b3d31e5ffc78
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-10 07:34:03 +0100

    Define explicit fields in serializers

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -6,10 +6,23 @@ from borrowing.models import Borrowing, Payments
 class BorrowingSerializer(serializers.Serializer):
     class Meta:
         model: Borrowing
-        fields = '__all__'
+        fields = (
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book_id",
+            "user_id",
+        )
 
 
 class PaymentsSerializers(serializers.Serializer):
     class Meta:
         model: Payments
-        fields = '__all__'
+        fields = (
+            "status",
+            "type",
+            "borrowing_id",
+            "session_url",
+            "session_id",
+            "money_to_pay",
+        )
