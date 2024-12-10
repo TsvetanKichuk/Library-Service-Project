0a00ed40bcef7be907f3b5e2ca24ab34f3896fb4
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-10 07:33:48 +0100

    Use ForeignKey for relational fields in models

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from rest_framework.authtoken.admin import User
+from book_app import Book
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book_id = models.IntegerField()
-    user_id = models.IntegerField()
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Payments(models.Model):
@@ -20,7 +22,7 @@ class Payments(models.Model):
     ]
     status = models.BooleanField(choices=STATUS_CHOICES)
     type = models.CharField(max_length=100, choices=TYPE_CHOICES)
-    borrowing_id = models.IntegerField()
+    borrowing_id = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
     session_url = models.URLField()
     session_id = models.CharField(max_length=100)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
