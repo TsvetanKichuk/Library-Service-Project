2423f21c3f994bc40cd0440e11d82bf313814a2c
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-10 22:10:17 +0100

    Switch BookSerializer to use ModelSerializer

--- book_app/serializers.py
+++ book_app/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from book_app.models import Book
 
 
-class BookSerializer(serializers.Serializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = (
