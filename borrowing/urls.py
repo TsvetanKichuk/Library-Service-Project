22e852df5537b31e0fa797fc7000507ea167759e
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-10 22:09:32 +0100

    Add PaymentsViewSet to borrowing app router

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from borrowing.views import BorrowingViewSet
+from borrowing.views import BorrowingViewSet, PaymentsViewSet
 
 app_name = "borrowing"
 
 router = routers.DefaultRouter()
 router.register("borrowings", BorrowingViewSet)
+router.register("payments", PaymentsViewSet)
 
 urlpatterns = [path("", include(router.urls))]
