8787265cd32677fc9e726c1ea632c2cfd5d603cd
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-10 18:56:24 +0100

    Add filters and schema documentation for BookViewSet

--- book_app/views.py
+++ book_app/views.py
@@ -1,3 +1,6 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import mixins
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 
@@ -11,8 +14,29 @@ class OrderPagination(PageNumberPagination):
     max_page_size = 100
 
 
-class BookViewSet(GenericViewSet):
+class BookViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
     pagination_class = OrderPagination
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "author",
+                type={"type": "list", "items": {"type": "number"}},
+                description="Filter by author id (ex. ?actors=2,5)",
+            ),
+            OpenApiParameter(
+                "title",
+                type=OpenApiTypes.STR,
+                description="Filter by book title (ex. ?title=fiction)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
