8733a8bc98bdac95baa244e723c19c399f41867a
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-12 11:56:31 +0100

    Register Payments model in Django admin

--- borrowing/admin.py
+++ borrowing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from borrowing.models import Borrowing
+from borrowing.models import Borrowing, Payments
 
 admin.site.register(Borrowing)
+admin.site.register(Payments)
