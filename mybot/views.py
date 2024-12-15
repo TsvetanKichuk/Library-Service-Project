4f42ccb774bc2e7eb5b61b0b562c97e7fbb66e47
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-15 14:36:19 +0100

    Use environment variable for Telegram bot token

--- mybot/views.py
+++ mybot/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from os import environ
 
-MY_TOKEN = "7966195586:AAGZo2C8RpUQQ289OHAkeftVKDqbs3kd-ns"
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+
+load_dotenv()
+
+MY_TOKEN = environ["TELEGRAM_TOKEN"]
 
 updater = Updater(token=MY_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello I'm your bot!")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Hello I'm your bot!"
+    )
 
 
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
