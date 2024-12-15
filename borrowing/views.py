ac31f0f4c8d712be17564d2d52da931d765854cc
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-15 14:49:32 +0100

    Refactor Borrowing and Payments viewsets to ModelViewSet

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,13 +1,36 @@
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from borrowing.models import Borrowing, Payments
-from borrowing.serializers import BorrowingSerializer, PaymentsSerializers
+from borrowing.serializers import BorrowingSerializer, PaymentsSerializer, BorrowingsDetailSerializer
 
 
-class BorrowingViewSet(viewsets.GenericViewSet):
-    queryset = Borrowing.objects.select_related("book_id, user")
+class BorrowingFilter(filters.FilterSet):
+    user_id = filters.NumberFilter(field_name='user__id')
+    is_active = filters.BooleanFilter(method='filter_is_active')
+
+    class Meta:
+        model = Borrowing
+        fields = ['user_id', 'is_active']
+
+    def filter_is_active(self, queryset, name, value):
+        if value:
+            return queryset.filter(returned_at__isnull=True)
+        return queryset.filter(returned_at__isnull=False)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related("user_id", "book_id")
     serializer_class = BorrowingSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_class = BorrowingFilter
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingSerializer
+        return BorrowingsDetailSerializer
 
     @staticmethod
     def post(request, *args, **kwargs):
@@ -31,6 +54,6 @@ class BorrowingViewSet(viewsets.GenericViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class PaymentsViewSet(viewsets.GenericViewSet):
-    queryset = Payments.objects.select_related("borrowing_id")
-    serializer_class = PaymentsSerializers
+class PaymentsViewSet(viewsets.ModelViewSet):
+    queryset = Payments.objects.select_related("borrowing_id__user_id")
+    serializer_class = PaymentsSerializer
