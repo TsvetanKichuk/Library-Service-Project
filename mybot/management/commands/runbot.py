7aa18c074f5fd349dd5e93b55ef0ed054a3ab33b
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-15 18:32:03 +0100

    Rename and restructure runbot management command directory

--- mybot/management/commands/runbot.py
+++ mybot/management/commands/runbot.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
+
 from mybot.views import updater
 
 
 class Command(BaseCommand):
-    help = 'Runs the Telegram bot.'
+    help = "Runs the Telegram bot."
 
     def handle(self, *args, **options):
         updater.start_polling()
