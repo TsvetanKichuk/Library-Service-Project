2f8e3338ae2c33cdf37c4e92ae019d8a8a91501a
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-15 14:50:07 +0100

    Refactor router and add PaymentsViewSet.

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
+from book_app.serializers import BookSerializer
 from borrowing.models import Borrowing, Payments
 
 
-class BorrowingSerializer(serializers.Serializer):
+class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
-        model: Borrowing
+        model = Borrowing
         fields = (
             "id",
             "borrow_date",
@@ -16,12 +19,44 @@ class BorrowingSerializer(serializers.Serializer):
         )
 
 
-class PaymentsSerializers(serializers.Serializer):
+def create(validated_data):
+    book = validated_data["book"]
+    if book.inventory < 1:
+        raise serializers.ValidationError("No more books available")
+    book.inventory -= 1
+    book.save()
+    borrowing = Borrowing.objects.create(**validated_data)
+    return borrowing
+
+
+def return_book(validated_data):
+    book = validated_data["book"]
+    borrowing = validated_data["borrowing"]
+    if borrowing.actual_return_date is not datetime.date.today():
+        return book.inventory
+    book.inventory += 1
+    book.save()
+
+
+class BorrowingsDetailSerializer(BorrowingSerializer):
+    inventory = BookSerializer(source="book_id", read_only=True, )
+
     class Meta:
-        model: Payments
+        model = Borrowing
         fields = (
-            "id",
-            "payment_date",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book_id",
+            "user_id",
+            "inventory",
+        )
+
+
+class PaymentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = (
             "status",
             "type",
             "borrowing_id",
