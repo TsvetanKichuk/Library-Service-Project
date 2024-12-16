8ac2a573592153a3b4dd46934889f2877d1b956a
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-16 08:47:30 +0100

    Removed the static `post` method from `BorrowingViewSet`

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,19 +1,22 @@
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from borrowing.models import Borrowing, Payments
-from borrowing.serializers import BorrowingSerializer, PaymentsSerializer, BorrowingsDetailSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    PaymentsSerializer,
+    BorrowingsDetailSerializer,
+)
 
 
 class BorrowingFilter(filters.FilterSet):
-    user_id = filters.NumberFilter(field_name='user__id')
-    is_active = filters.BooleanFilter(method='filter_is_active')
+    user_id = filters.NumberFilter(field_name="user_id__id", lookup_expr="exact")
+    is_active = filters.BooleanFilter(method="filter_is_active")
 
     class Meta:
         model = Borrowing
-        fields = ['user_id', 'is_active']
+        fields = ["user_id", "is_active"]
 
     def filter_is_active(self, queryset, name, value):
         if value:
@@ -31,27 +34,6 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return BorrowingSerializer
         return BorrowingsDetailSerializer
-
-    @staticmethod
-    def post(request, *args, **kwargs):
-        serializer = BorrowingSerializer(data=request.data)
-
-        if serializer.is_valid():
-            book = serializer.validated_data["book_id"]
-
-            if book.inventory > 0:
-                book.inventory -= 1
-                book.save()
-
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(
-                    {"error": "Book is not available"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
