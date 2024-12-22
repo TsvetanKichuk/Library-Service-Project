61473a79554ec00a633b20c85047f21247ce0a5f
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-22 15:14:09 +0100

    Improve Stripe integration with customer management

--- borrowing/server.py
+++ borrowing/server.py
@@ -1,15 +1,23 @@
+import os
+
 import stripe
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 stripe.api_key = os.environ["STRIPE_SECRET_KEY"]
 
+stripe.Customer.create(
+    name="Tsvetan Kichuk",
+    email="toys4babyodua@gmail.com",
+)
+
 stripe.PaymentIntent.create(
-    amount=1000,
+    customer='{{CUSTOMER_ID}}',
+    amount=1099,
     currency="usd",
-    stripe_account="{{CONNECTED_ACCOUNT_ID}}",
+    setup_future_usage="off_session",
+    automatic_payment_methods={"enabled": True},
 )
 
 stripe.Balance.retrieve(stripe_account='{{CONNECTED_ACCOUNT_ID}}')
@@ -23,3 +31,4 @@ stripe.Customer.delete(
     "{{CUSTOMER_ID}}",
     stripe_account='{{CONNECTED_ACCOUNT_ID}}',
 )
+
