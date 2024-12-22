b27d2523b8354f25f1f0ee69b69746ddaa67f5ab
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-22 15:17:48 +0100

    Add function to send Telegram notifications

--- mybot/views.py
+++ mybot/views.py
@@ -1,17 +1,25 @@
-from os import environ
+import os
+
+from telegram import Bot
 
 from dotenv import load_dotenv
+
 from telegram.ext import Application, CommandHandler
 
 load_dotenv()
 
-MY_TOKEN = environ["TELEGRAM_TOKEN"]
+MY_TOKEN = os.environ["TELEGRAM_TOKEN"]
 
 application = Application.builder().token(MY_TOKEN).build()
 
 
 async def start(update, context):
     await update.message.reply_text("Hello I'm your bot!")
+
+
+def send_telegram_notification(chat_id, message):
+    bot = Bot(token=MY_TOKEN)
+    bot.send_message(chat_id=chat_id, text=message)
 
 
 start_handler = CommandHandler("start", start)
