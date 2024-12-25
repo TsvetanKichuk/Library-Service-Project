5446781421d10231fe499fe71f6e77ef9ef105d7
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-25 19:04:22 +0100

    Remove PaymentsViewSet and its route from URLs

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from borrowing.views import BorrowingViewSet, PaymentsViewSet
+from borrowing.views import BorrowingViewSet
 
 app_name = "borrowing"
 
 router = routers.DefaultRouter()
 router.register("", BorrowingViewSet)
-router.register("payments", PaymentsViewSet)
 urlpatterns = [path("", include(router.urls))]
