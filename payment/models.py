6a8f48b9f59a07c20ca7f617a9c2baf2d301b221
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-26 18:52:52 +0100

    Remove commented-out Stripe session creation code

--- payment/models.py
+++ payment/models.py
@@ -24,34 +24,3 @@ class Payments(models.Model):
 
     def __str__(self):
         return f"{self.status}, {self.type}, {self.borrowing_id}"
-
-# @receiver(post_save, sender=Payments)
-# def create_stripe_session(sender, instance, created, **kwargs):
-#     if created:
-#         try:
-#             with transaction.atomic():
-#                 session = stripe.checkout.Session.create(
-#                     payment_method_types=["card"],
-#                     mode="payment",
-#                     line_items=[
-#                         {
-#                             "price_data": {
-#                                 "currency": "usd",
-#                                 "product_data": {
-#                                     "name": f"Payment for borrowing {instance.borrowing_id.book_id.title}",
-#                                 },
-#                                 "unit_amount": int(instance.money_to_pay * 100),  # Stripe требует сумму в центах
-#                             },
-#                             "quantity": 1,
-#                         }
-#                     ],
-#                     success_url="https://buy.stripe.com/test_28ocN89fyfPM9NK8wx",
-#                     cancel_url="https://buy.stripe.com/test_28ocN89fyfPM9NK8wx/cancel",
-#                 )
-#
-#                 instance.session_url = session.url
-#                 instance.session_id = session.id
-#                 instance.save()
-#         except Exception as e:
-#             print(f"Error creating stripe session: {e}")
-#             raise
