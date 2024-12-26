87b72518271799d9eb1469d1150cb5b848bafc93
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-26 18:48:29 +0100

    Enable signals import in Borrowing app ready method.

--- borrowing/apps.py
+++ borrowing/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BorrowingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "borrowing"
+
+    def ready(self):
+        try:
+            import borrowing.signals
+        except ImportError as e:
+            raise ImportError(f"Error importing signals module: {e}")
