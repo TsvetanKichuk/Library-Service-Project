f6a022450cf60b1d9401af375ce69b513966fda7
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-26 18:55:28 +0100

    Add overdue borrowing notification functionality

--- borrowing/signals.py
+++ borrowing/signals.py
@@ -1,12 +1,14 @@
 import asyncio
+
 from django.conf import settings
-import json
-import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.timezone import now
+
 from telegram import Bot
-from payment.models import Payments
+
 from borrowing.models import Borrowing
+from payment.models import Payments
 
 MY_TOKEN = settings.TELEGRAM_BOT_API_KEY
 CHAT_ID = settings.CHAT_ID
@@ -38,7 +40,28 @@ def notify_new_borrowing(sender, instance, created, **kwargs):
 def notify_successful_payment(sender, instance, created, **kwargs):
     if created:
         message = (
-                f"Payment Successful: Payment of {instance.money_to_pay} "
-                f"for borrowing '{instance.borrowing_id.book_id.title}' was successful."
-            )
+            f"Payment Successful: Payment of {instance.money_to_pay} "
+            f"for borrowing '{instance.borrowing_id.book_id.title}' was successful."
+        )
         send_telegram_notification(chat_id=CHAT_ID, message=message)
+
+
+def check_overdue_borrowings():
+    """
+    Check borrowings and send notifications for overdue ones.
+    """
+    overdue_borrowings = Borrowing.objects.filter(
+        returned_at__isnull=True, expected_return_date__lt=now()
+    )
+
+    for borrowing in overdue_borrowings:
+        message = (
+            f"Overdue Borrowing Alert:\n"
+            f"Book: {borrowing.book_id.title}\n"
+            f"Due Date: {borrowing.expected_return_date}\n"
+            f"Please return your book to avoid penalties."
+        )
+        try:
+            send_telegram_notification(borrowing.user_id.email, message)
+        except Exception as e:
+            print(f"Error while sending overdue borrowing notification: {e}")
