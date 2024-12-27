6c201e53a31cb70a4be759f43756e1b0300150b3
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-27 07:24:21 +0100

    Added a Celery shared task to notify users about overdue borrowings via Telegram.

---
+++ borrowing/tasks.py
@@ -0,0 +1,28 @@
+from celery import shared_task
+from dateutil.utils import today
+from django.db.models import Q
+
+from borrowing.models import Borrowing  # Замените на вашу модель Borrowing
+from borrowing.signals import send_telegram_notification
+
+
+@shared_task
+def check_overdue_borrowings():
+    """
+    Check borrowings and send notifications for overdue ones.
+    """
+    overdue_borrowings = Borrowing.objects.filter(
+        Q(expected_return_date__lte=today) & Q(returned_at__isnull=True)
+    )
+
+    for borrowing in overdue_borrowings:
+        message = (
+            f"Overdue Borrowing Alert:\n"
+            f"Book: {borrowing.book_id.title}\n"
+            f"Due Date: {borrowing.expected_return_date}\n"
+            f"Please return your book to avoid penalties."
+        )
+        try:
+            send_telegram_notification(borrowing.user_id.email, message)
+        except Exception as e:
+            print(f"Error while sending overdue borrowing notification: {e}")
