15cd6bbd13831f5709595f3eae93470db1207f45
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-29 14:36:53 +0100

    Add return_borrowing method to handle book returns

--- borrowing/models.py
+++ borrowing/models.py
@@ -16,7 +16,10 @@ class Borrowing(models.Model):
     def __str__(self):
         return f"{self.book_id} - {self.user_id}"
 
-    def save(self, *args, **kwargs):
-        if self.expected_return_date < self.borrow_date:
-            raise ValueError("Expected return date cannot be before borrow date.")
-        super().save(*args, **kwargs)
+    def return_borrowing(self):
+        if self.actual_return_date:
+            raise ValueError("This borrowing has already been returned.")
+        self.actual_return_date = datetime.date.today()
+        self.book_id.inventory += 1  # Increment book inventory
+        self.book_id.save()
+        self.save()
