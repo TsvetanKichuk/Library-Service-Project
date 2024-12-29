b9b49b26568a0a41dec949104f8a4777f3584061
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-29 14:37:15 +0100

    Refactor permission class and standardize string quotes.

--- payment/views.py
+++ payment/views.py
@@ -3,31 +3,31 @@ from django.conf import settings
 from django.http import JsonResponse
 from rest_framework import viewsets
 
-from borrowing.permissions import IsAdminOrOwner
+from borrowing.permissions import IsAdminUserOrReadOnly
 from payment.models import Payments
 from payment.serializers import PaymentsSerializer
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-YOUR_DOMAIN = 'http://127.0.0.1:8000'
+YOUR_DOMAIN = "http://127.0.0.1:8000"
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
     queryset = Payments.objects.select_related("borrowing_id__user_id")
     serializer_class = PaymentsSerializer
-    permission_classes = [IsAdminOrOwner]
+    permission_classes = [IsAdminUserOrReadOnly]
 
     def post(self, request, *args, **kwargs):
         checkout_session = stripe.checkout.Session.create(
             line_items=[
                 {
-                    'price': 'price_1QZsmuRwyw2Mr9VCS6s4VbFs',
-                    'quantity': 1,
+                    "price": "price_1QZsmuRwyw2Mr9VCS6s4VbFs",
+                    "quantity": 1,
                 },
             ],
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success/',
-            cancel_url=YOUR_DOMAIN + '/cancel/',
+            mode="payment",
+            success_url=YOUR_DOMAIN + "/success/",
+            cancel_url=YOUR_DOMAIN + "/cancel/",
         )
         return JsonResponse({"id": checkout_session.id})
