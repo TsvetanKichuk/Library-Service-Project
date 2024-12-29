45b98b9ee74df64fe9df543633d3c571566a60ca
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-12-29 14:32:42 +0100

    Add IsAdminUserOrReadOnly permission class

--- borrowing/permissions.py
+++ borrowing/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, IsAuthenticated
 
 
 class IsAdminOrOwner(BasePermission):
@@ -9,3 +9,10 @@ class IsAdminOrOwner(BasePermission):
         if request.user.is_staff:
             return True
         return obj.borrowing_id.user_id == request.user
+
+
+class IsAdminUserOrReadOnly(IsAuthenticated):
+    def has_permission(self, request, view):
+        if request.method in ["GET", "HEAD", "OPTIONS"]:
+            return True
+        return request.user.is_staff
